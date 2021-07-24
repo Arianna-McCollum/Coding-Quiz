acd90c12b9f6a3f7daabd2643287a30078513791
Electrolion <AMccollum71@gmail.com>
2021-07-24 00:31:54 -0400

    Enhancement: added functional high score section

--- assets/score.js
+++ assets/score.js
@@ -1,21 +1,28 @@
 const initialInput = document.querySelector('#ininput');
 const myForm = document.querySelector('#my-form');
 const userList = document.querySelector('.ranking-text');
-var userScore = 0;
+var userScore = [];
 var scores= JSON.parse(localStorage.getItem("scores"))
 if (scores===null){
     scores= []; 
 }
+ 
 
     var score = { initials: initialInput.value, value : userScore};
     scores.push(score);
-    console.log(userList);
-    localStorage.setItem("scores", JSON.stringify(scores));//local storage to save score to computer
+    localStorage.getItem("scores", JSON.stringify(scores));//local storage to save score to computer
     for(var i = 0; i< scores.length; i++){
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(`${scores[i].initials} : ${scores[i].value}` ));
         console.log(li);
         userList.appendChild(li);
+        
+
     }
+
+    var list = document.getElementById("myList");
+    list.removeChild(list.childNodes[i]); 
+
+    //Let's add local storage refresh to a clear score button + css sound effects? 
     
     
